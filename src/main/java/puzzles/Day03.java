3502f932ee0b6f12e79aa9701e37642e0c77d4c1
Jonathan Kraska <jkraska@appnexus.com>
2018-12-03 10:44:35 -0500

    Day 3 part 2 question added

--- src/main/java/puzzles/Day03.java
+++ src/main/java/puzzles/Day03.java
@@ -162,6 +162,16 @@ public class Day03 extends PuzzleDay {
   }
 
 
+  /**
+   * --- Part Two ---
+   * Amidst the chaos, you notice that exactly one claim doesn't overlap by even a single square
+   * inch of fabric with any other claim. If you can somehow draw attention to it, maybe the
+   * Elves will be able to make Santa's suit after all!
+   *
+   * For example, in the claims above, only claim 3 is intact after all claims are made.
+   *
+   * What is the ID of the only claim that doesn't overlap?
+   */
   @Override
   public void solvePart2() {
     Fabric fabric = new Fabric(1000, 1000);
